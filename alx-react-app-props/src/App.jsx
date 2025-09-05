9f5f804f026861c5b18a07198b5fa1c8d45ad060
not-zendaya <ukzendaya@gmail.com>
2025-09-05 21:47:24 +0300

    Fix:

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import ProfilePage from './components/ProfilePage'
-import UserContext from './UserContext'
+import UserContext from './components/UserContext'
 
 function App() {
   const [count, setCount] = useState(0)
