a081aad12af67a10e6aa7b7d5634186c3e904b64
not-zendaya <ukzendaya@gmail.com>
2025-09-06 11:01:01 +0300

    Initial setup of company website with React Router and basic pages

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home (){
+    return(
+        <div style ={{padding: '20px'}}>
+            <h1>Welcome to Our Company</h1>
+            <p>We are dedicated to delivering excellence in al serveices.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        backgroundColor: "navy",
+        padding: "10px",
+        textAlign: "center"
+      }}
+    >
+      <Link to="/" style={{ margin: "0 15px", color: "white" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "0 15px", color: "white" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ margin: "0 15px", color: "white" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ margin: "0 15px", color: "white" }}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
