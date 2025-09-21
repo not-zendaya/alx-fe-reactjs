a35fa2689499cd93d6316efb24f1dcd97a0ba656
not-zendaya <ukzendaya@gmail.com>
2025-09-21 15:17:20 +0300

    fix: update

--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -1,6 +1,6 @@
 function Home (){
     return(
-        <div style ={{padding: '20px'}}>
+        <div style ={{padding: '20px', margin: "0 auto"}}>
             <h1>Welcome to Our Company</h1>
             <p>We are dedicated to delivering excellence in al serveices.</p>
         </div>
