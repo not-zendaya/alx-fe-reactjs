54bba0c819f84bad886c15b811e86cfc98fe33da
not-zendaya <ukzendaya@gmail.com>
2025-09-21 17:18:08 +0300

    feat: add advanced search with location and minimum repo filters

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get (`https://api.github.com/users/${username}`);
-  return response.data;
+export const fetchUserData = async (username, location, repos) => {
+  let query = "";
+
+  if (username) query += `${username} `;
+  if (location) query += `location:${location} `;
+  if (repos) query += `repos:>=${repos}`;
+
+  const url = `https://api.github.com/search/users?q=${query.trim()}`;
+  const response = await axios.get(url);
+  return response.data.items;
 };
