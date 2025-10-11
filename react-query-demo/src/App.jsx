8e9c8b0650c3b94c88b7bea8a9179142389d9c8f
not-zendaya <ukzendaya@gmail.com>
2025-10-11 14:49:19 +0300

    fix: import file path

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import PostsComponent from "./PostsComponent";
+import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
 
